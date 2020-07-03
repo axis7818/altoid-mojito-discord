cc81dbe35d03bbe1f9de12858735e0940fdfdaa1
Cameron Taylor <axis7818@gmail.com>
2020-07-03 16:09:41 -0700

    latest docker tag

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 version=$(jq -r '.version' package.json)
-docker build . -t direwolfcr.azurecr.io/altoid-mojito-discord:${version}
+docker build . -t direwolfcr.azurecr.io/altoid-mojito-discord:${version} -t direwolfcr.azurecr.io/altoid-mojito-discord:latest
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,3 +2,4 @@
 
 version=$(jq -r '.version' package.json)
 docker push direwolfcr.azurecr.io/altoid-mojito-discord:${version}
+docker push direwolfcr.azurecr.io/altoid-mojito-discord:latest
