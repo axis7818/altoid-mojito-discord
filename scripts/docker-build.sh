4e5f73792c7f8463bcd26a3c6b562e5df20994ac
Cameron Taylor <axis7818@gmail.com>
2023-08-13 22:40:11 +0000

    specify platform

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 version=$(jq -r '.version' package.json)
-docker build . \
+docker build --platform linux/amd64 . \
 	-t direwolfcr.azurecr.io/altoid-mojito-discord:${version} \
 	-t direwolfcr.azurecr.io/altoid-mojito-discord:latest
